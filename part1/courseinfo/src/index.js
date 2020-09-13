852350d1fd8f3add2aa5d2d4bdad815e9771c565
dzabeligan <dzabeligan@gmail.com>
2020-09-13 18:16:19 +0100

    part 1b courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -11,35 +11,42 @@ const Part = (props) => (
 
 const Content = (props) => (
   <>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
+    {props.parts.map((part) => (
+      <Part part={part.name} exercises={part.exercises} />
+    ))}
   </>
 );
 
 const Total = (props) => <p>Number of exercises {props.total}</p>;
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  function getTotalForObjectFieldInArray(array, field) {
+    return array.map((item) => item[field]).reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={getTotalForObjectFieldInArray(course.parts, 'exercises')} />
     </>
   );
 };
